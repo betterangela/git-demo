85f5fb283ed6dd413c7968d8568f65fef143f397
v_lkeqiliu <v_lkeqiliu@tencent.com>
2021-12-14 18:14:07 +0800

    feat:tail

--- hello.vue
+++ hello.vue
@@ -11,4 +11,4 @@ hello git ! hello vim !ddddd
 hello git ! hello vim !
 hello git ! hello vim ! 3333333333333333
 hello git ! hello vim ! 2222222222222222222222
-hello git ! hello vim ! 11111111111111111111
+hello git ! hello vim ! 11111111111111111111111
